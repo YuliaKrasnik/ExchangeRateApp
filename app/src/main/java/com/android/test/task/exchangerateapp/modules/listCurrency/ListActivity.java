cf5052dbe35801da3debafc050ea02385c4dbee4
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-09-11 20:09:41 +0700

    added fragment in ListActivity

--- app/src/main/java/com/android/test/task/exchangerateapp/modules/listCurrency/ListActivity.java
+++ app/src/main/java/com/android/test/task/exchangerateapp/modules/listCurrency/ListActivity.java
@@ -1,16 +1,33 @@
 package com.android.test.task.exchangerateapp.modules.listCurrency;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.android.test.task.exchangerateapp.R;
 
 public class ListActivity extends AppCompatActivity {
-
+    IListModuleContract.IListView view;
+    Fragment fragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (null == fragment) {
+            fragment = new ListFragment();
+            fragmentTransaction
+                    .add(R.id.fragment_container, fragment)
+                    .commit();
+        }
+        view = (IListModuleContract.IListView) fragment;
+
     }
 }
--- app/src/main/java/com/android/test/task/exchangerateapp/modules/listCurrency/ListFragment.java
+++ app/src/main/java/com/android/test/task/exchangerateapp/modules/listCurrency/ListFragment.java
@@ -1,6 +1,7 @@
 package com.android.test.task.exchangerateapp.modules.listCurrency;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,16 +9,32 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+import com.android.test.task.exchangerateapp.R;
 
 public class ListFragment extends Fragment implements IListModuleContract.IListView {
+    private IListModuleContract.IListPresenter presenter;
+    private ListAdapter adapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    
     @Override
     public void setPresenter(IListModuleContract.IListPresenter presenter) {
-        
+        this.presenter = presenter;
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        return view;
     }
 }
