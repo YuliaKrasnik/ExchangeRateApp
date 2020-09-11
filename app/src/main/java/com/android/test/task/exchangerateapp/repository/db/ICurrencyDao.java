06ed0852f39c30823c86c0b0ffa00eba8da836f9
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-09-11 21:09:06 +0700

    write received json into db

--- app/src/main/java/com/android/test/task/exchangerateapp/repository/db/ICurrencyDao.java
+++ app/src/main/java/com/android/test/task/exchangerateapp/repository/db/ICurrencyDao.java
@@ -1,7 +1,16 @@
 package com.android.test.task.exchangerateapp.repository.db;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+
+import com.android.test.task.exchangerateapp.model.modelDb.Currency;
+import com.android.test.task.exchangerateapp.model.modelDb.UpdateDate;
 
 @Dao
 public interface ICurrencyDao {
+    @Insert
+    void insertDate(UpdateDate updateDate);
+
+    @Insert
+    void insertCurrency(Currency currency);
 }
