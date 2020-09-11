1559931f4d41db31a714d91ad1d29b660da14284
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-09-11 20:19:31 +0700

    create useCase class

---
+++ app/src/main/java/com/android/test/task/exchangerateapp/useCase/UseCase.java
@@ -0,0 +1,42 @@
+package com.android.test.task.exchangerateapp.useCase;
+
+public abstract class UseCase<RequestParametersType extends UseCase.IRequestValues,
+        ResponseType extends UseCase.IResponseValues> {
+
+    private RequestParametersType requestParameters;
+    private IUseCaseCallback<ResponseType> callback;
+
+    protected abstract void execute(final RequestParametersType requestParameters);
+
+    public interface IRequestValues {
+    }
+
+    public interface IResponseValues {
+    }
+
+    public interface IUseCaseCallback<T> {
+        void onSuccess(final T response);
+
+        void onError(final String errorMessage);
+    }
+
+    void run() {
+        execute(requestParameters);
+    }
+
+    public RequestParametersType getRequestParameters() {
+        return requestParameters;
+    }
+
+    void setRequestParameters(final RequestParametersType requestParameters) {
+        this.requestParameters = requestParameters;
+    }
+
+    public IUseCaseCallback<ResponseType> getCallback() {
+        return callback;
+    }
+
+    void setCallback(final IUseCaseCallback<ResponseType> callback) {
+        this.callback = callback;
+    }
+}
