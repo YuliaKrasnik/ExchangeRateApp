8aa969b2fc755ed36f0e4bf030e5fffb0d11c0c4
Yulia Krasnik <i.krasnik@g.nsu.ru>
2020-09-13 21:23:34 +0700

    added methods in converter contract

--- app/src/main/java/com/android/test/task/exchangerateapp/modules/listCurrency/IConverterModuleContract.java
+++ app/src/main/java/com/android/test/task/exchangerateapp/modules/listCurrency/IConverterModuleContract.java
@@ -1,15 +1,27 @@
 package com.android.test.task.exchangerateapp.modules.listCurrency;
 
+import com.android.test.task.exchangerateapp.model.modelDb.Currency;
 import com.android.test.task.exchangerateapp.modules.common.IBasePresenter;
 import com.android.test.task.exchangerateapp.modules.common.IBaseView;
 
 
 public interface IConverterModuleContract {
     interface IConverterView extends IBaseView<IConverterModuleContract.IConverterPresenter> {
+        void onItemSelected(Currency currency);
+
+        void onSwipeRefresh();
+
+        void setCountedValue(String value);
+
+        void setNameCurrency(String name);
 
     }
 
     interface IConverterPresenter extends IBasePresenter {
+        void onItemSelected(Currency currency);
 
+        void onSwipeRefresh();
+
+        void onClick(String valueEditText);
     }
 }
